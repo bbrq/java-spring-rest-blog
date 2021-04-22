e0f80922f99162c4e20c2c481db46cf679cfc578
Ashley <sophiawu2016@gmail.com>
2021-04-22 16:01:31 -0400

    Removing Author from the API

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -1,11 +1,15 @@
 package com.pluralsight.blog.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 import com.pluralsight.blog.model.Author;
 
 @Component
+//can't browse the authors at all except to be displayed within a Post
+//check by localhost:8080
+@RepositoryRestResource(exported=false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     
 }

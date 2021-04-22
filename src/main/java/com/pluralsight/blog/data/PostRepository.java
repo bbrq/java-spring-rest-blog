dfc3c5dfa9bb31815203c284e6ee6f73f8dd981e
Ashley <sophiawu2016@gmail.com>
2021-04-22 14:24:46 -0400

    rename Default Search Name

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.blog.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.pluralsight.blog.model.Post;
 
@@ -11,5 +12,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	//check query definition by: http://localhost:8080/posts/search ->
 	//http://localhost:8080/posts/search/findByTitleContaining{?title}"
 	//http://localhost:8080/posts/search/findByTitleContaining?title=Smart
+	@RestResource(rel="contains-title", path="containsTitle")
+	//http://localhost:8080/posts/search/containsTitle?title=Smart
 	List<Post> findByTitleContaining(String title);
 }

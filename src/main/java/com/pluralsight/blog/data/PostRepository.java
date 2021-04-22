570f3cca1ccd7ce2ae6173ac8a19997660e172ce
Ashley <sophiawu2016@gmail.com>
2021-04-22 14:22:08 -0400

    Add Search by Title Containing

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,10 +1,15 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.pluralsight.blog.model.Post;
 
 //to create the REST API endpoints
 public interface PostRepository extends JpaRepository<Post, Long> {
+	//check query definition by: http://localhost:8080/posts/search ->
+	//http://localhost:8080/posts/search/findByTitleContaining{?title}"
+	//http://localhost:8080/posts/search/findByTitleContaining?title=Smart
+	List<Post> findByTitleContaining(String title);
 }
